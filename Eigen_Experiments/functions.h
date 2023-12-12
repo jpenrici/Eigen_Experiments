6f0b57857cf6f34e8f0f8e182790564c0849a2c6
Juan Pablo Enrici <jpenrici@gmail.com>
2023-12-12 16:17:58 -0300

    Update.

--- Eigen_Experiments/functions.h
+++ Eigen_Experiments/functions.h
@@ -2,6 +2,8 @@
 #define FUNCTIONS_H
 
 #include <eigen3/Eigen/Core>
+#include <opencv4/opencv2/opencv.hpp>
+
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -9,22 +11,22 @@
 #include <string>
 
 
-auto rows(const auto &mat) -> std::string
+auto rows(const auto &matrix) -> std::string
 {
-    return std::to_string(mat.rows());
+    return std::to_string(matrix.rows());
 }
 
-auto cols(const auto &mat) -> std::string
+auto cols(const auto &matrix) -> std::string
 {
-    return std::to_string(mat.cols());
+    return std::to_string(matrix.cols());
 }
 
-auto shape(const auto &mat) -> std::string
+auto shape(const auto &matrix) -> std::string
 {
-    return "(" + rows(mat) + ", " + cols(mat) + ")";
+    return "(" + rows(matrix) + ", " + cols(matrix) + ")";
 }
 
-auto toStr(const auto &mat, std::string label = "Matrix") -> std::string
+auto toStr(const auto &matrix, std::string label = "Matrix") -> std::string
 {
     std::stringstream ss;
     Eigen::IOFormat HeavyFmt(Eigen::FullPrecision, 0, ", ", ",\n", "[", "]", "[", "]");
@@ -34,43 +36,130 @@ auto toStr(const auto &mat, std::string label = "Matrix") -> std::string
            << label                          << "\n";
     }
 
-    ss << "Shape:" << shape(mat)         << "\n"
-       << "Size :" << mat.size()         << "\n"
-       << std::string(label.size(), '-') << "\n"
-       << mat.format(HeavyFmt)           << "\n";
+    ss << "Shape: " << shape(matrix)         << "\n"
+       << "Size : " << matrix.size()         << "\n"
+       << std::string(label.size(), '-')     << "\n"
+       << matrix.format(HeavyFmt)            << "\n";
 
     return ss.str();
 }
 
-void view(const auto &mat, std::string label = "")
+void view(const auto &matrix, std::string label = "")
 {
-    std::cout << toStr(mat, label);
+    std::cout << toStr(matrix, label);
 }
 
-void fit(auto &mat, std::function<double(double, double, double)> func)
+void fit(auto &matrix, std::function<double(double, double, double)> func)
 {
-    for (int row = 0; row < mat.rows(); ++row) {
-        for (int col = 0; col < mat.cols(); ++col) {
-            mat(row, col) = func(row, col, 0.1);
+    for (int row = 0; row < matrix.rows(); ++row) {
+        for (int col = 0; col < matrix.cols(); ++col) {
+            matrix(row, col) = func(row, col, 0.1);
         }
     }
 }
 
 template<typename T>
-void border(auto &matrix, const auto &mat, T value, int width = 1)
+void border(auto &matrix, const auto &matrixBase, T value, int width = 1)
 {
     if (width < 1) {
         std::cerr << "Invalid border width!\n";
         return;
     }
 
-    matrix.resize(mat.rows() +  2 * width, mat.cols() + 2 * width);
+    matrix.resize(matrixBase.rows() +  2 * width, matrixBase.cols() + 2 * width);
     matrix.setConstant(value);
-    for (int row = 0; row < mat.rows(); ++row) {
-        for (int col = 0; col < mat.cols(); ++col) {
-            matrix(row + width, col + width) = mat(row, col);
+    for (int row = 0; row < matrixBase.rows(); ++row) {
+        for (int col = 0; col < matrixBase.cols(); ++col) {
+            matrix(row + width, col + width) = matrixBase(row, col);
         }
     }
+}
+
+void vecStr2matrix(auto &matrix, std::vector<std::vector<std::string> > vector2D)
+{
+    if (vector2D.empty()) {
+        std::cerr << "Vector 2D of strings is empty!\n";
+        return;
+    }
+
+    // Analyze
+    int maxCols{0};
+    for (auto row : vector2D) {
+        maxCols = row.size() >= maxCols ? row.size() : maxCols;
+        for (auto col : row) {
+        }
+    }
+
+    // Fill
+    matrix.resize(vector2D.size(), maxCols);
+    matrix.setZero();
+    for (int row = 0; row < vector2D.size(); ++row) {
+        for (int col = 0; col < vector2D.at(row).size(); ++col) {
+            try {
+                matrix(row, col) = stod(vector2D[row][col]);
+            }
+            catch (...) {
+                // Fill in with zero if non-numeric value.
+                matrix(row, col) = 0;
+            }
+        }
+    }
+}
+
+void cv2eigen(auto &matrix, const cv::Mat &matrixBase)
+{
+    int rows = matrixBase.rows;
+    int cols = matrixBase.cols;
+    int channels = matrixBase.channels();
+    int dims = matrixBase.dims; // Dimensionality.
+
+    if (dims > 2) {
+        std::cerr << "Conversion not recommended!\n";
+        return;
+    }
+
+    matrix.resize(rows, cols * channels);
+    matrix.setZero();
+    for (int row = 0; row < rows; ++row) {
+        auto *ptr = matrixBase.ptr(row);
+        for (int col = 0; col < cols * channels; ++col) {
+            matrix(row, col) = *ptr++;
+        }
+    }
+}
+
+auto loadCSV(const std::string &filename, char delimiter = ',') -> std::vector<std::vector<std::string> >
+{
+    std::vector<std::vector<std::string> > table;
+    try {
+        std::ifstream fIn(filename, std::ios::in);
+        if (fIn.is_open()) {
+            std::string line{};
+            while (getline(fIn, line)) {
+                std::vector<std::string> row;
+                std::string str{};
+                for (auto character : line) {
+                    if (character == delimiter) {
+                        row.emplace_back(str);
+                        str.clear();
+                    }
+                    else {
+                        str.push_back(character);
+                    }
+                }
+                if (!str.empty()) {
+                    row.emplace_back(str);
+                }
+                table.emplace_back(row);
+            }
+            fIn.close();
+        }
+    }
+    catch (...) {
+        std::cerr << "There was something wrong while loading the file!\n";
+    }
+
+    return table;
 }
 
 void save(const std::string &filename, std::string text, bool view = true)
@@ -83,7 +172,7 @@ void save(const std::string &filename, std::string text, bool view = true)
     try {
         std::fstream fs(filename, std::fstream::in | std::fstream::out | std::fstream::app);
         if (fs.is_open()) {
-            fs << text;
+            fs << text; // Append text at the end of the file.
         }
         fs.close();
         if (view) {
@@ -94,6 +183,15 @@ void save(const std::string &filename, std::string text, bool view = true)
     catch (...) {
         std::cerr << "There was something wrong while saving the file!\n";
     }
+}
+
+void saveCSV(const auto &matrix, const std::string &filename, char delimiter = ',')
+{
+    std::stringstream ss;
+    Eigen::IOFormat csvFmt(Eigen::FullPrecision, 0, {delimiter}, "\n");
+    ss << matrix.format(csvFmt);
+
+    save(filename, ss.str(), false);
 }
 
 #endif // FUNCTIONS_H
--- Eigen_Experiments/main.cpp
+++ Eigen_Experiments/main.cpp
@@ -1,20 +1,71 @@
 #include "functions.h"
 
 
-void test();
+void basic_test();
+void csv_test();
+void opencv_test();
 
 auto main() -> int
 {
     std::cout << "Simple Help/Test with Eigen ...\n";
 
-    test();
+    basic_test();
+    csv_test();
+    opencv_test();
 
     return EXIT_SUCCESS;
 }
 
-void test()
+void opencv_test()
+{
+    cv::Mat m1;
+    m1.create(2, 4, CV_8UC(5));
+    for (int i = 0; i < m1.rows * m1.cols * m1.channels(); ++i) {
+        m1.at<uchar>(i) = i + 1;
+    }
+
+    auto fmt = cv::Formatter::FMT_PYTHON;
+    std::cout << format(m1, fmt) << "\n";
+    std::cout << "Shape: (" << m1.rows << ", " << m1.cols << ", " << m1.channels() << ")\n";
+
+    Eigen::MatrixXd m2;
+    cv2eigen(m2, m1);
+    view(m2, "MatrixXd");
+
+    cv::Mat image;
+    image = cv::imread("Resources/image.png");
+
+    std::cout << "Image:\n";
+    //std::cout << format(image, fmt) << "\n";
+    std::cout << "Shape: (" << image.rows << ", " << image.cols << ", " << image.channels() << ")\n";
+
+    //cv::imshow("Display Image", image);
+    //cv::waitKey(0);
+
+    cv2eigen(m2, image);
+    save("image.txt", toStr(m2, "image.png"), false);
+    saveCSV(m2, "image.csv");
+}
+
+void csv_test()
+{
+    auto csv = loadCSV("Resources/table.csv");
+
+    Eigen::MatrixXi m1;
+    vecStr2matrix(m1, csv);
+    view(m1, "MatrixXi");
+
+    Eigen::MatrixXd m2;
+    vecStr2matrix(m2, csv);
+    view(m2, "MatrixXd");
+
+    saveCSV(m2, "output.csv");
+}
+
+void basic_test()
 {
     Eigen::VectorXd v1(3);
+    v1.setOnes();
     view(v1);
     save("output.txt", toStr(v1, "VectorXd"));
     save("output.txt", toStr(v1.transpose(), "VectorXd.transpose"));
@@ -49,4 +100,3 @@ void test()
     border(m3, v2.transpose(), 0);
     save("output.txt", toStr(m3, "MatrixXd.transpose"));
 }
-
